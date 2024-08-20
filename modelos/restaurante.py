7ba5ca50c5e9f408bbbbbdc04d256f0dc3942169
Jean <jeanmatheus0612@gmail.com>
2024-08-20 16:06:13 -0300

    Usando o método __str__

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,10 +4,13 @@ class Restaurante:
         self.categoria = categoria
         self.ativo = False
 
+    def __str__(self) -> str:
+        return f'{self.nome} | {self.categoria}'
+
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(vars(restaurante_praca))
-print(vars(restaurante_pizza))
+print((restaurante_praca))
+print((restaurante_pizza))
