ff929a5e4ac892caebabdf09ccfcfa4433ad7518
Jean <jeanmatheus0612@gmail.com>
2024-08-20 16:31:34 -0300

    Criando o meu próprio método para listar os restaurantes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,16 +1,19 @@
 class Restaurante:
+    restaurantes = []
     def __init__(self, nome, categoria) -> None:  # método construtor __init__
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self) -> str:
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f"{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}")
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza express', 'Italiana')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
-
-print((restaurante_praca))
-print((restaurante_pizza))
+Restaurante.listar_restaurantes()
