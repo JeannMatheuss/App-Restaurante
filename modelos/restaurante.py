4b2f9ede0ffa8d336e6efb4f2d4cda4c92b80ef8
Jean <jeanmatheus0612@gmail.com>
2024-08-20 17:49:28 -0300

    Criando o método alternar_estado do restaurante

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -9,15 +9,18 @@ class Restaurante:
     def __str__(self) -> str:
         return f'{self._nome} | {self._categoria}'
     
-    
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f"{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo.ljust(25)}")
 
     @property
     def ativo(self):
         return '☒' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza express', 'Italiana')
