389d2ec6eb4fb3ce5895539ae46f04648e0ba39a
Jean <jeanmatheus0612@gmail.com>
2024-08-20 15:39:40 -0300

    Criando a classe Restaurante

---
+++ modelos/restaurante.py
@@ -0,0 +1,14 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
+
+
+restaurante_pizza = Restaurante()
+
+restauntes = [restaurante_praca, restaurante_pizza]
+print(dir(restauntes))
