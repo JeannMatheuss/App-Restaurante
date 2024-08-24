ec28341ed253fe2f6683c395bb5f5de59ca47348
Jean <jeanmatheus0612@gmail.com>
2024-08-23 22:40:14 -0300

    Acessando os itens do cardápio

--- modelos/cardapio/bebida.py
+++ modelos/cardapio/bebida.py
@@ -4,3 +4,6 @@ class Bebida(itemCardapio):
     def __init__(self, nome, preco, tamanho):
         super().__init__(nome, preco)
         self._tamanho = tamanho
+
+    def __str__(self) -> str:
+        return self._nome
--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -4,3 +4,6 @@ class Prato(itemCardapio):
     def __init__(self, nome, preco, descricao) -> None:
         super().__init__(nome, preco)
         self._descricao = descricao
+
+    def __str__(self) -> str:
+        return self._nome
