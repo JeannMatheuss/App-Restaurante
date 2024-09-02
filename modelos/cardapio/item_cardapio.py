0f41e10b8d924cd41036803cab2b9d63eb91d2d5
Jean <jeanmatheus0612@gmail.com>
2024-09-02 09:47:47 -0300

    Corrigindo todos os bugs e aplicando o desconto

--- modelos/cardapio/item_cardapio.py
+++ modelos/cardapio/item_cardapio.py
@@ -1,4 +1,10 @@
-class itemCardapio:
-    def __init__(self, nome, preco) -> None:
+from abc import ABC, abstractmethod
+
+class ItemCardapio(ABC):
+    def __init__(self,nome,preco):
         self._nome = nome
         self._preco = preco
+
+    @abstractmethod
+    def aplicar_desconto(self):
+        pass
